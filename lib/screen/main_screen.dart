ff278508a3e9dbe3fa081764390b811cb2f6ce34
JJY <wjdwlsdyd77@gmail.com>
2025-06-29 14:17:14 +0900

    섹션 8 : 상태관리 기초 까지 완료

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -8,55 +8,34 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  TextEditingController idController = TextEditingController();
+  String msg = '이 곳에 입력 값이 업데이트 됩니다!';
+  ValueNotifier<int> counter = ValueNotifier<int>(0);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('메인화면')),
       body: Column(
-        // 세로로 정렬하는 위젯
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('반갑습니다.'),
-          Text('저는 유데미 강의를 듣고있는 학생입니다.'),
-          Text('저는 홍길동입니다.'),
-          // ctrl + ., mac -> cmd+ .
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end, // 가로로 위젯을 쌓아서 정렬하는 위젯
-            children: [Text('안녕'), Text('반가워'), Text('이것은 가로 방향으로 쌓는 위젯이야')],
+          TextField(
+            controller: idController,
+            decoration: InputDecoration(labelText: '아이디를 입력해주세요'),
           ),
-          Row(
-            children: [
-              Expanded(flex: 2, child: Text('홍길동')),
-              Expanded(child: Text('홍길동')),
-              Expanded(child: Text('홍길동')),
-            ],
+          ElevatedButton(
+            onPressed: () {
+              // //클릭 시 동작 구현!
+              // setState(() {
+              //   msg = idController.text.toString();
+              // });
+              counter.value = 30;
+            },
+            child: Text('아이디 입력 값 가져오기 !'),
           ),
-          Container(
-            width: 300,
-            height: 100,
-            margin: EdgeInsets.only(left: 16),
-            alignment: Alignment.bottomCenter,
-            child: Text('홍드로이드'),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.blue,
-            ),
-          ),
-          Text(
-            '홍드로이드2',
-            style: TextStyle(
-              color: Colors.green,
-              fontWeight: FontWeight.bold,
-              fontSize: 30,
-            ),
-          ),
-          Row(
-            children: [
-              Image.asset('assets/car.png', width: 100, height: 100,),
-              Icon(Icons.home_filled, size: 100,)
-            ],
-          )//image
+          ValueListenableBuilder<int>(valueListenable: counter, builder: (context, value, child){
+            return Text('Count : $value');
+          },),
+
+          Text(msg, style: TextStyle(fontSize: 30)),
         ],
       ),
     );
